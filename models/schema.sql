cbc5c6a22acf3d49ff3f78a2b2fa19176ca62489
pablodiazt <pablodiaztr@gmail.com>
2019-07-15 13:58:39 -0500

    db

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,40 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+CREATE DATABASE crypter;
+USE crypter;
+
+CREATE TABLE user (
+    id int AUTO_INCREMENT NOT NULL,
+	username varchar(100) NOT NULL,
+    password varchar(100),
+    usd_balance int,
+    coins_owned int,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE coin (
+    id int AUTO_INCREMENT NOT NULL,
+	price int NOT NULL,
+    coin_rank int,
+    username varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+          
+INSERT INTO user
+    (username)
+VALUES
+    ('Dr. McGregor');
+
+INSERT INTO user
+    (password)
+VALUES
+    ('YaBabyMama123');
+
+INSERT INTO user
+    (usd_balance, coins_owned)
+VALUES
+    (32500, 2);   
+
+select * from user
