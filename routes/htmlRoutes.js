6696f8a062202a72fd8dd9e5464c8d14b9a93662
Anderson <andersonswaimboyd@gmail.com>
2019-07-17 14:18:12 -0500

    resolving conflicts

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,26 @@
 var db = require("../models");
 var passport = require("../config/passport");
-var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", isAuthenticated, function(req, res) {
-    if(!isAuthenticated){
-      res.redirect("/login");
-    }
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/", function(req, res) {
+    res.render("index", {msg:"Hello!"});
   });
 
-  app.post("/register", function(req, res){
+  app.get("/register", function(req, res){
     res.render("register");
   });
 
-  app.post("/login", passport.authenticate("local"), {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
+  app.get("/login", function(req, res){
+    res.render("login");
+  });
+
+  app.post("/login", passport.authenticate("local"), function(cb){
+    cb({
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true
+    });
   });
 
   // Load example page and pass in an example by id
