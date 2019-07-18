e003cec301aea6aca08bfaaa858bcb1ac8e5a40c
Anderson <andersonswaimboyd@gmail.com>
2019-07-18 13:09:22 -0500

    added user / coin page to display on findOne

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -35,8 +35,8 @@ module.exports = function(app) {
   });
 
   app.get("/coins/:name", function(req, res){
-    db.Coin.findOne({ where: { name: req.params.name} }).then(function(dbCoin){
-      res.render("coins", {coins: dbCoin});
+    db.Coin.findOne({ where: {name: req.params.name} }).then(function(dbCoin){
+      res.render("coinPage", {coin: dbCoin});
     });
   });
 
@@ -48,7 +48,7 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/users/:id", function(req, res) {
     db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
-      res.render("users", {
+      res.render("profile", {
         user: dbUser
       });
     });
